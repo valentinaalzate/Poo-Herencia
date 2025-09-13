ef7e1ce0a672e6c385f860ac441a19c8391eefbd
valentinaalzate <vaev39@gmail.com>
2025-09-13 11:55:10 -0500

    edit

--- Poo-Herencia/PooHerencia.core/Trapeze.cs
+++ Poo-Herencia/PooHerencia.core/Trapeze.cs
@@ -16,12 +16,12 @@ namespace PooHerencia.core
             set => _d = ValidateD(value);
         }
 
-        public override double GetArea() => ((B + D) * H) / 2;
+        public override double GetArea() => (B + D) * H / 2;
 
         public override double GetPerimeter() => A + B + C + D;
 
 
-        public Trapeze(string name, double a, double b, double c, double h, double d) : base(name, a, b, c, h)
+        public Trapeze(string name, double a, double b,  double c, double d, double h) : base(name, a, b, c, h)
         {
             D = d;
         }
